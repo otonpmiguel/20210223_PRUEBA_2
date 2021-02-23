b2d0fae298677b5bad5be5be4a4b835e94986378
otonpmiguel <oton.pmiguel@alumnos.upm.es>
2021-02-23 17:44:59 +0100

    Añado el cálculo de factorial de 10

--- codigo.c
+++ codigo.c
@@ -4,5 +4,18 @@ int main(){
 
 printf("hello world");
 
+int a = 1; 
+int i = 10; 
+
+//numero factorial
+
+for (i=10; i>=1; i--){
+
+    a = a*i;
+}
+
+printf("/n El factorial de 10 es: %d  I es igual a: %d", a, i);
+
 return 0;
 };
+
